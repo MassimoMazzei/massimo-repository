1d319e73b7f051441283a33c579230229ba3f2de
Massimo <mas.mazzei@gmail.com>
2017-06-01 15:04:14 +0200

    Wifi enabled

--- app/src/main/java/it/alephzero/romatrewifi/LoginActivity.java
+++ app/src/main/java/it/alephzero/romatrewifi/LoginActivity.java
@@ -1,13 +1,55 @@
 package it.alephzero.romatrewifi;
 
+import android.content.Context;
+import android.net.wifi.WifiConfiguration;
+import android.net.wifi.WifiManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import com.android.volley.RequestQueue;
+
+import java.util.List;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String URL_LOGIN = "https://authentication.uniroma3.it:8003";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
     }
+
+    public void login(View view) {
+        EditText username = (EditText) findViewById(R.id.usernameText);
+        EditText password = (EditText) findViewById(R.id.passwordText);
+
+        //enable wifi connection
+        WifiManager wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        wifiManager.setWifiEnabled(true);
+
+        //connect to Rm3Wi-Fi
+        String networkSSID = "Rm3Wi-Fi";
+        WifiConfiguration conf = new WifiConfiguration();
+        conf.SSID = "\"" + networkSSID + "\"";
+        conf.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+        wifiManager.addNetwork(conf);
+
+        List<WifiConfiguration> list = wifiManager.getConfiguredNetworks();
+        for( WifiConfiguration i : list ) {
+            if(i.SSID != null && i.SSID.equals("\"" + networkSSID + "\"")) {
+                wifiManager.disconnect();
+                wifiManager.enableNetwork(i.networkId, true);
+                wifiManager.reconnect();
+
+                break;
+            }
+        }
+
+        RequestQueue
+
+    }
 }
+
